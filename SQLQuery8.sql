476beb7413293fce8c6fe46a3a636144d00a4fa1
Vedran Džanko <vedran.dzanko@gmail.com>
2023-05-19 21:52:53 +0200

    Create SQLQuery8.sql
    Zadaća

---
+++ SQLQuery8.sql
@@ -0,0 +1,5 @@
+select * from polaznik;
+update polaznik set prezime = 'Herc' where sifra = 18;
+
+update polaznik set email = 'ibicak@ht.hr' where sifra = 10;
+update polaznik set oib = '25698545854' where sifra = 10;
